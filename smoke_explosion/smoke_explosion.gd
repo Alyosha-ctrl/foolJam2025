68764e590845def5c4e50505cb0016fe56101f3d
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-23 17:49:56 -0700

    Turned off physics lerp for explosions

--- smoke_explosion/smoke_explosion.gd
+++ smoke_explosion/smoke_explosion.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 
 func _ready():
-	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
 	%Smoke.material.set_shader_parameter("texture_offset", Vector2(randfn(0.0, 1.0), randfn(0.0, 1.0)))
 	%AnimationPlayer.play("explosion")
 	await %AnimationPlayer.animation_finished

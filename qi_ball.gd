c02e19522827b2f8f2f4eb998f8b13cb6138e162
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-27 10:21:51 -0700

    Removed text, made qi_balls slightly bigger

--- qi_ball.gd
+++ qi_ball.gd
@@ -14,26 +14,7 @@ func _on_body_entered(body: Node2D) -> void:
 		player.add_exp(exp_amount)
 		queue_free()
 		
-func set_exp_amount(newExp):
+func set_exp_amount(newExp : float) -> void:
 	exp_amount = newExp
-	#Code that would be helpful, but might impact performance
-	%exp_amount_text.text = str(exp_amount)
-	if(exp_amount >= 100):
-		%Octogan.scale = Vector2(2,2)
-	elif(exp_amount >= 1000):
-		%Octogan.scale = Vector2(3,3)
-		%Grab_Area.scale = Vector2(2,2)
-	elif(exp_amount >= 10000):
-		%Octogan.scale = Vector2(3.5,3.5)
-		%Grab_Area.scale = Vector2(2.5,2.5)
-	elif(exp_amount >= 100000):
-		%Octogan.scale = Vector2(4,4)
-		%Grab_Area.scale = Vector2(3,3)
-	elif(exp_amount >= 1000000):
-		%Octogan.scale = Vector2(4.5,4.5)
-		%Grab_Area.scale = Vector2(3.5,3.5)
-	elif(exp_amount >= 10000000):
-		%Octogan.scale = Vector2(5,5)
-		%Grab_Area.scale = Vector2(4,4)
 	
 		
